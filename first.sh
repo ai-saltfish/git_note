c7434e5d01328b56514adb520b82d0d223c68b71
wjamx <10841764@qq.com>
2022-02-13 15:03:18 +0800

    modify first

---
+++ first.sh
@@ -0,0 +1,15 @@
+# git全局配置
+git config --global user.name 'username'
+git config --global user.email 'email'
+
+# git 仓库配置
+git init
+
+# 多个文件提交暂存区
+git add -u
+# 单个文件夹/文件提交暂存区
+git add filename
+
+# 提交仓库
+git commit -m 'first commit'
+
